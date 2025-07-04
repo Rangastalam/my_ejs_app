82e1ed7370e5be46ca9ed3e3326873cf9ffbe4e2
Rishi Karthik <karthikrishi91@gmail.com>
2025-07-04 14:11:43 +0530

    final commit

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+const data=[
+  {
+    title: "Season-1",
+    content: "This is a sample post content."
+  },
+  {
+    title: "Season-2",
+    content: "This is a sample post content."
+  },
+  {
+    title: "Season-3",
+    content: "This is a sample post content."
+  },
+  {
+    title: "Season-4",
+    content: "This is a sample post content."
+  },
+  {
+    title: "Season-5",
+    content: "This is a sample post content."
+  },
+];
+var newdata=[];
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-  res.render("index.ejs");
+  res.render("index.ejs",{newposts: newdata});
+  
 });
 
 app.get("/compose",(req,res)=>{
   res.render("compose.ejs");
 });
 
-app.get("")
-// app.get("/contact", (req, res) => {
-//   res.render("contact.ejs");
-// });
+app.post("/add",(req,res)=>{
+  const newpost={
+    title:req.body.title,
+    content:req.body.content
+  }
+  newdata.push(newpost);
+  data.push(newpost);
+  res.redirect("/");
+
+});
+app.get("/posts/:postName",(req,res)=>{
+  const requestedTitle = req.params.postName;
+  data.forEach((post) => {
+    if (post.title === requestedTitle) {
+      res.render("post.ejs", { postTitle: post.title, postContent: post.content });
+    }
+  });
+});
 
 
 
