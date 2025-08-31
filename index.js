57d8adda48589a5b789a6c855be9b1aebccb1ebd
Rishi Karthik <karthikrishi91@gmail.com>
2025-08-31 13:00:41 +0530

    changed login page

--- index.js
+++ index.js
@@ -38,175 +38,183 @@ app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const data = [
-  {
-    title: "Season-1",
-    content: "This is a sample post content."
-  },
 
-];
 var newdata = [];
 
 app.use(flash());
-app.get("/",(req,res)=>{
+// app.get("/",(req,res)=>{
 
 
-  res.render("index.ejs",[blogCategories,blog_data]);
+//   res.render("index.ejs",[blogCategories,blog_data]);
 
-});
-// app.get("/", (req, res) => {
-//   if (req.isAuthenticated()) {
-//     res.render("index.ejs", { newposts: data });
-//   } else {
-//     res.redirect("/login");
-//   }
 // });
+// app.get("/post/:postId",(req,res)=>{
+//   var postid=req.params.postId;
 
-// app.get("/login", (req, res) => {
+//   var post=blog_data.filter((post)=>post._id===postid);
+//   var title=post[0].title;
+//   var description=post[0].description;
+//   var comments=null;
+//   res.render('post.ejs',{postId:postid,title,description,comments})
+  
+  
+
+
+
+// });
+app.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("index.ejs",[blogCategories,blog_data]);
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/login", (req, res) => {
  
   
-//   res.render("login.ejs",{ message: req.flash("error") });
-// }); 
+  res.render("login.ejs",{ message: req.flash("error") });
+}); 
 
-// app.get("/compose", (req, res) => {
-//   res.render("compose.ejs");
-// });
-// app.get("/auth/google", passport.authenticate("google", {
-//   scope: ["profile", "email"],
-// }));
+app.get("/compose", (req, res) => {
+  res.render("compose.ejs");
+});
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"],
+}));
 
-// app.get("/auth/google/heisenBlog",passport.authenticate("google",{
-//   successRedirect:"/",
-//   failureRedirect:"/login",
-// }));
+app.get("/auth/google/heisenBlog",passport.authenticate("google",{
+  successRedirect:"/",
+  failureRedirect:"/login",
+}));
 
-// app.get("/register", (req, res) => {
-//   res.render("register.ejs", { message: req.flash("error") });
-// });
-// app.post("/register", async (req, res) => {
-//   const email = req.body.username;
-//   const password = req.body.password;
+app.get("/register", (req, res) => {
+  res.render("register.ejs", { message: req.flash("error") });
+});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
-//   try {
-//     const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-//       email,
-//     ]);
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
-//     if (checkResult.rows.length > 0) {
-//       req.flash("error", "User already exists. Please log in.");
-//       return res.redirect("/register");
-//     } else {
-//       bcrypt.hash(password, saltRounds, async (err, hash) => {
-//         if (err) {
-//           req.flash("error", "Error hashing password.");
-//           return res.redirect("/register");
-//         } else {
-//           const result = await db.query(
-//             "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
-//             [email, hash]
-//           );
-//           const user = result.rows[0];
-//           req.login(user, (err) => {
-//             if (err) {
-//               req.flash("error", "Registration succeeded but login failed.");
-//               return res.redirect("/register");
-//             }
-//             res.redirect("/");
-//           });
-//         }
-//       });
-//     }
-//   } catch (err) {
-//     req.flash("error", "Server error during registration.");
-//     res.redirect("/register");
-//   }
-// });
-// app.post("/login",passport.authenticate("local",{
-//   successRedirect: "/",
-//   failureRedirect: "/login",
-//   failureFlash: true,
+    if (checkResult.rows.length > 0) {
+      req.flash("error", "User already exists. Please log in.");
+      return res.redirect("/register");
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          req.flash("error", "Error hashing password.");
+          return res.redirect("/register");
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            if (err) {
+              req.flash("error", "Registration succeeded but login failed.");
+              return res.redirect("/register");
+            }
+            res.redirect("/");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    req.flash("error", "Server error during registration.");
+    res.redirect("/register");
+  }
+});
+app.post("/login",passport.authenticate("local",{
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
 
-// }));
-// app.post("/add", (req, res) => {
-//   const newpost = {
-//     title: req.body.title,
-//     content: req.body.content
-//   }
-//   newdata.push(newpost);
-//   data.push(newpost);
-//   res.redirect("/");
+}));
+app.post("/add", (req, res) => {
+  const newpost = {
+    title: req.body.title,
+    content: req.body.content
+  }
+  newdata.push(newpost);
+  data.push(newpost);
+  res.redirect("/");
 
-// });
-// app.get("/posts/:postName", (req, res) => {
-//   const requestedTitle = req.params.postName;
-//   data.forEach((post) => {
-//     if (post.title === requestedTitle) {
-//       res.render("post.ejs", { postTitle: post.title, postContent: post.content });
-//     }
-//   });
-// });
+});
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = req.params.postName;
+  data.forEach((post) => {
+    if (post.title === requestedTitle) {
+      res.render("post.ejs", { postTitle: post.title, postContent: post.content });
+    }
+  });
+});
 
 
-// passport.use("local", new Strategy(async function verify(username, password, cb) {
-//   try {
-//       const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
-//         username,
-//       ]);
-//       if (result.rows.length > 0) {
-//         // console.log("User found:", result.rows[0]);
-//         const user = result.rows[0];
-//         const storedHashedPassword = user.password;
-//         bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-//           if (err) {
-//             //Error with password check
-//             console.error("Error comparing passwords:", err);
-//             return cb("Server error");
-//           } else {
-//             if (valid) {
-//               //Passed password check
-//               return cb(null, user);
-//             } else {
-//               //Did not pass password check
-//               return cb(null,false, { message: "Incorrect username or password." });
-//             }
-//           }
-//         });
-//       } else {
+passport.use("local", new Strategy(async function verify(username, password, cb) {
+  try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        // console.log("User found:", result.rows[0]);
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            //Error with password check
+            console.error("Error comparing passwords:", err);
+            return cb("Server error");
+          } else {
+            if (valid) {
+              //Passed password check
+              return cb(null, user);
+            } else {
+              //Did not pass password check
+              return cb(null,false, { message: "Incorrect username or password." });
+            }
+          }
+        });
+      } else {
 
-//         return cb(null, false, { message: "user not found try registering first" });
-//       }
-//     } catch (err) {
-//       cb("Server error: " );
-//       console.log(err);
-//     }
+        return cb(null, false, { message: "user not found try registering first" });
+      }
+    } catch (err) {
+      cb("Server error: " );
+      console.log(err);
+    }
 
-// }));
-// passport.use("google", new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: "/auth/google/heisenBlog",
-// }, async (accessToken, refreshToken, profile, cb) => {
-//   console.log(profile);
-//   try {
-//     const result = await db.query("SELECT * FROM users WHERE email = $1", [profile.email]);
-//     if (result.rows.length === 0) {
-//       const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [profile.email, "google"]);
-//       return cb(null, newUser.rows[0]);
-//     }
-//     else {
-//       const user = result.rows[0];
-//       return cb(null, user);
-//     }
-//   } catch (err) {
-//     return cb(err);
-//   }
-// }));
-// passport.serializeUser((user, cb) => {
-//   cb(null, user);
-// });
-// passport.deserializeUser((user, cb) => {
-//   cb(null, user);
-// });
+}));
+passport.use("google", new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/heisenBlog",
+}, async (accessToken, refreshToken, profile, cb) => {
+  console.log(profile);
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [profile.email]);
+    if (result.rows.length === 0) {
+      const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [profile.email, "google"]);
+      return cb(null, newUser.rows[0]);
+    }
+    else {
+      const user = result.rows[0];
+      return cb(null, user);
+    }
+  } catch (err) {
+    return cb(err);
+  }
+}));
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
